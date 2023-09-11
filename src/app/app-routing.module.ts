b3ffb0ad32ec3fbde0f3ad527f29ac275ca48c0a
mahmoudHeshame <mahmoudheshame809@gmail.com>
2023-09-11 22:36:15 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about' ,component:AboutComponent,pathMatch:'full' },
+  {path:'portfolio' ,component:PortfolioComponent },
+  {path:'contact' ,component:ContactComponent },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
